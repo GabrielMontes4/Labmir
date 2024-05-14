1078276cbd253235719e163b9bee3721f22795d0
GabrielMontes4 <josegabrielmontesmarcano@gmail.com>
2024-05-14 15:03:39 -0400

    Delete prueba directory

--- prueba/main.js
+++
@@ -1,6 +0,0 @@
-let myClassicEditor;
-
-ClassicEditor
-.create( document.querySelector('#descripcion'))
-.then(editor => {myClassicEditor = editor})
-.catch( error => {console.log(error)});
--- prueba/validar_producto.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(empty($_SESSION["id"])){
-    //header("Location: productos.php");
-    $id2 = $_SESSION["id"];
-    echo $id2 . "dcs";
-   // exit(); // Finalizamos el script despues de redirigir!
-};
-?>
